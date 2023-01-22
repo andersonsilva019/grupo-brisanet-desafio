6bef43d2b598c54a43f15611361b68c1790c91ac
Anderson <anderson@pop-os.localdomain>
2023-01-22 09:19:15 -0300

    style: configurando as cores do tema

---
+++ src/styles/index.ts
@@ -0,0 +1,11 @@
+import { createStitches } from '@stitches/react'
+
+export const { globalCss, styled } = createStitches({
+  theme: {
+    colors: {
+      'gray-100': '#E1E1E6',
+      'gray-800': '#121214',
+      'white': '#FFF'
+    }
+  }
+})
