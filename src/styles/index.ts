d442a339382971c7d3d74a7dd859bf01c96e21d3
Anderson <anderson@pop-os.localdomain>
2023-01-22 09:42:37 -0300

    style: adicionando novas cores ao tema

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4,6 +4,9 @@ export const { globalCss, styled } = createStitches({
   theme: {
     colors: {
       'gray-100': '#E1E1E6',
+      'gray-600': '#7C7C8A',
+      'ghray-500': '#323238',
+      'gray-700': '#202024',
       'gray-800': '#121214',
       'white': '#FFF'
     }
