6a4f923fb1ec0154d086faadc3a5cc0d4ce8250e
Anderson <anderson@pop-os.localdomain>
2023-01-23 15:05:09 -0300

    style: adicionando nova cor ao tema

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,13 +1,14 @@
 import { createStitches } from '@stitches/react'
 
-export const { globalCss, styled } = createStitches({
+export const { globalCss, styled, keyframes } = createStitches({
   theme: {
     colors: {
       'gray-100': '#E1E1E6',
       'gray-600': '#7C7C8A',
-      'ghray-500': '#323238',
+      'gray-500': '#323238',
       'gray-700': '#202024',
       'gray-800': '#121214',
+      'green-500': '#00875F',
       'white': '#FFF'
     }
   }
