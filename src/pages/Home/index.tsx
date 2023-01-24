2d12999b4c12681af155eedcad73989c64c502fc
Anderson <anderson@pop-os.localdomain>
2023-01-24 09:27:57 -0300

    feat: melhorando usabilidade da pagina Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,30 +1,63 @@
+import 'rc-pagination/assets/index.css';
+import Pagination from 'rc-pagination';
 import { Comic } from '../../components/Comic'
+import { ErrorComponent } from '../../components/Errors'
 import { Modal } from '../../components/Modal'
+import { Spinner } from '../../components/Spinner'
 import { useComics } from '../../services/marvelApi/marvelApiSerivices'
 import * as S from './styles'
+import { useState } from 'react';
 
 export function Home() {
+  const [currentPage, setCurrentPage] = useState(1)
 
-  const { data } = useComics()
+  const {
+    data,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useComics({ page: currentPage })
+
+  function handlePageChange(page: number) {
+    setCurrentPage(page)
+  }
 
   return (
     <S.Container>
-      <h1>Projeto Front-End | Grupo Brisanet</h1>
-      <S.GridContainer>
-        {data?.map((comic) => (
-          <Modal
-            key={comic.id}
-            comicId={comic.id}
-            trigger={
-              <Comic
-                title={comic.title}
-                imageUrl={comic.imageUrl}
-                creators={comic.creators as string}
+      <h1>Comics</h1>
+      {isLoading && !isError && <Spinner color="#00875F" />}
+
+      {isError && !isLoading && !isSuccess && <ErrorComponent />}
+
+      {!isError && !isLoading && isSuccess && (
+        <S.Content>
+          <S.GridContainer>
+            {data?.comics.map((comic) => (
+              <Modal
+                key={comic.id}
+                comicId={comic.id}
+                trigger={
+                  <Comic
+                    title={comic.title}
+                    imageUrl={comic.imageUrl}
+                    creators={comic.creators as string}
+                  />
+                }
               />
-            }
-          />
-        ))}
-      </S.GridContainer> 
+            ))}
+          </S.GridContainer>
+          <S.PaginationContainer>
+            <Pagination
+              pageSize={20}
+              total={data.totalOfComics}
+              showSizeChanger
+              onChange={handlePageChange}
+              current={currentPage}
+              locale={{ items_per_page: 'Comics por página' }}
+            />
+          </S.PaginationContainer>
+        </S.Content>
+      )}
     </S.Container>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,6 +2,7 @@ import { styled } from '../../styles'
 
 export const Container = styled('div', {
   width: '100%',
+  height: '100%',
   maxWidth: '1220px',
   margin: '0 auto 3rem',
 
@@ -11,9 +12,91 @@ export const Container = styled('div', {
   }
 })
 
+export const Content = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+})
+
 export const GridContainer = styled('div', {
   marginTop: '2rem',
   display: 'grid',
   gridTemplateColumns: 'repeat(5, 1fr)',
   gap: '0.5rem'
+})
+
+export const PaginationContainer = styled('div', {
+  margin: '2rem 0 10rem',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+
+  '.rc-pagination-next': {
+    backgroundColor: '$gray-500',
+    borderColor: '$gray-500',
+
+    '&:hover, &:focus': {
+      borderColor: '$green-500',
+      color: '$white',
+      
+      button: {
+        borderColor: '$green-500',
+        backgroundColor: 'inherit',
+        color: 'inherit',
+      }
+    },
+
+    button: {
+      backgroundColor: 'inherit',
+      borderColor: 'inherit',
+      color: '$white',
+    }
+  },
+
+  '.rc-pagination-prev': {
+    backgroundColor: '$gray-500',
+    borderColor: '$gray-500',
+
+    '&:hover, &:focus': {
+      borderColor: '$green-500',
+      color: '$white',
+
+      button: {
+        backgroundColor: 'inherit',
+        borderColor: 'inherit',
+        color: 'inherit',
+      }
+    },
+
+
+    button: {
+      backgroundColor: 'inherit',
+      borderColor: 'inherit',
+      color: '$white',
+    }
+  },
+
+  '.rc-pagination-item': {
+    backgroundColor: '$gray-500',
+    borderColor: '$gray-500',
+    borderRadius: 6,
+
+    '&:hover, &:focus': {
+      borderColor: '$green-500',
+      color: '$white',
+
+      a: {
+        color: '$white',
+      }
+    },
+
+    a: {
+      color: '$white',
+    }
+  },
+
+  '.rc-pagination-item-active': {
+    backgroundColor: '$green-500',
+    borderColor: '$green-500',
+  }
 })
